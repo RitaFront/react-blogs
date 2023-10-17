346acfe1f2f154796adfc9b483f8b771a49e0920
margarita <ritatokareva99@mail.ru>
2023-10-17 16:24:03 +0300

    подключение bootstrap,react-bootstrap

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import './index.scss';
 
 function App() {
-  return <div className="App">hi</div>;
+  return (
+    <div>
+      <Button variant="primary">KNOPKA</Button>
+    </div>
+  );
 }
 
 export default App;
